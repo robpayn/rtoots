8bc0372fcff8e2fc335b9031ebc406c47538eed3
Rob Payn <robpayn@gmail.com>
2022-06-04 12:14:00 -0600

    Unique file names
    Added switch to turn unique file names for
    compiled files on and off.

--- markdown/graphics/base/02_protocol/compile.R
+++ markdown/graphics/base/02_protocol/compile.R
@@ -1,14 +1,29 @@
-rmarkdown::render(
-  input = "./02_protocol/graphics.Rmd",
-  output_format = "word_document",
-  output_dir = "./03_incremental",
-  output_file = sprintf(
+unique = FALSE
+
+wordFileName <- "graphics.docx"
+htmlFileName <- "graphics.html"
+if (unique) {
+  wordFileName <- sprintf(
     "graphics_%s.docx",
     format(
       x = Sys.time(),
       format = "%Y%m%d_%H%M%S"
     )
-  ),
+  )
+  htmlFileName <- sprintf(
+    "graphics_%s.html",
+    format(
+      x = Sys.time(),
+      format = "%Y%m%d_%H%M%S"
+    )
+  )
+}
+
+rmarkdown::render(
+  input = "./02_protocol/graphics.Rmd",
+  output_format = "word_document",
+  output_dir = "./03_incremental",
+  output_file = wordFileName,
   knit_root_dir = "."
 )
 
@@ -16,12 +31,6 @@ rmarkdown::render(
   input = "./02_protocol/graphics.Rmd",
   output_format = "html_document",
   output_dir = "./04_product",
-  output_file = sprintf(
-    "graphics_%s.html",
-    format(
-      x = Sys.time(),
-      format = "%Y%m%d_%H%M%S"
-    )
-  ),
+  output_file = htmlFileName,
   knit_root_dir = "."
 )
